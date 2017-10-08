068ec95abc855ce4d74b8f4703d77b656f3a7e45
peterliuxb <woairuidaren@gmail.com>
2017-10-08 15:57:39 +0800

    modified Quiz component

--- components/Quiz.js
+++ components/Quiz.js
@@ -4,13 +4,94 @@ import {View, Text, TouchableOpacity, StyleSheet} from 'react-native'
 class Quiz extends Component {
 
   state = {
+    showAnswer: false,
+    currentCardIdx:0,
+    corrects:0,
+    incorrects:0
+  };
 
+  const CORRECT = 'correct'
+  const INCORRECT = 'incorrect';
+
+
+  onShowPress = () => {
+    this.setState((state) => ({ showAnswer: !state.showAnswer }));
+  }
+
+  onNextPress = (answer) => {
+
+    if(answer === CORRECT) {
+      this.setState((state) => ({
+        showAnswer:false,
+        currentCardIdx: state.currentCardIdx + 1,
+        corrects: state.corrects + 1
+      }));
+    } else if (answer === INCORRECT) {
+      this.setState((state) => ({
+        showAnswer:false,
+        currentCardIdx: state.currentCardIdx + 1,
+        incorrects: state.incorrects + 1
+      }));
+    }
   }
 
   render() {
+
+    const flipTo = {
+      answer: 'Answer',
+      question: 'Question'
+    };
+
+    const {
+      showAnswer,
+      currentCardIdx,
+      corrects,
+      incorrects
+    } =  this.state
+    const { deck } = this.props.navigation.state.params
+    const currentCard = deck.questions[currentCardIdx]
+    const numberOfQuestions = deck.questions.length
+
+    //questions of cards are zero
+    if(numberOfQuestions === 0) {
+      return(
+        <View style={styles.container}>
+          <Text>No cards in this deck</Text>
+        </View>
+      );
+    }
+    //the last question
+    if(currentCardIdx === numberOfQuestions){
+      const percentage = (corrects/numberOfQuestions) * 100
+
+      return (
+        <View style={styles.container}>
+          <Text>{`You got ${percentage}% correct`}</Text>
+        </View>
+      )
+    }
+
     return(
       <View style={styles.container}>
-        <Text></Text>
+        <Text>{`${currentCardIdx + 1}/${numberOfQuestions}`}</Text>
+
+        {showAnswer ?
+          <Text style={styles.mainText}>{currentCard.answer}</Text>
+          :
+          <Text style={styles.mainText}>{currentCard.question}</Text>
+        }
+
+        <TouchableOpacity onPress={this.onShowPress}>
+          <Text style={styles.flipText}>Flip to {showAnswer ? flipTo.question : flipTo.answer}</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.correctButton} onPress={() => this.onNextPress(CORRECT)}>
+          <Text style={styles.buttonText}>Correct</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.incorrectButton} onPress={() => this.onNextPress(INCORRECT)}>
+          <Text style={styles.buttonText}>Incorrect</Text>
+        </TouchableOpacity>
       </View>
     )
   }
