38ff4b4e57f3598810557b1535ddaa5a8113accd
peterliuxb <woairuidaren@gmail.com>
2017-10-08 15:12:41 +0800

    add Card component

---
+++ components/AddCard.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+import {FormLabel, FormInput, FormValidationMessage, Button} from 'react-native-elements'
+import { addCardToDeck } from '../utils/helper'
+
+class AddCard extends Component {
+
+  static navigationOptions = {
+      tabBarLabel: 'Add Card',
+  };
+
+  state = {
+    question:'',
+    answer:''
+   }
+
+   onTextChange = (type,text) => {
+     this.setState(() => (
+       {
+         [type]:text
+       }
+     ))
+   }
+
+   submit = () => {
+     const { question, answer } = this.state
+     const { navigate } = this.props.navigation
+     const {deckTitle} = this.props.navigation.state.params
+
+     addCardToDeck(deckTitle, {question, answer}).then(
+       (data) => navigate('DeckList')
+     )
+   }
+
+  render() {
+    const { question, answer } = this.state
+
+    return(
+        <View>
+          <FormLabel>Question</FormLabel>
+          <FormInput onChangeText={(text) => this.onTextChange('question', text)} value={question}/>
+
+          <FormLabel>Answer</FormLabel>
+          <FormInput onChangeText={(text) => this.onTextChange('answer', text)} value={answer}/>
+          <button title="Submit" disabled={!(question && answer)} onPress={this.submit} />
+        </View>
+    )
+  }
+}
