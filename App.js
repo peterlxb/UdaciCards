f3634ac6885cd52f7e1f8466768af95b1cf6030d
peterliuxb <woairuidaren@gmail.com>
2017-10-27 13:30:31 +0800

    add new comp TabsComponent

--- App.js
+++ App.js
@@ -1,60 +1,7 @@
 import React,{Component} from 'react';
 import { StyleSheet, Text, View,Platform,StatusBar } from 'react-native';
-import AddNewDeck from './components/AddNewDeck'
-import AllDecks from './components/AllDecks'
-import DeckDetail from './components/DeckDetail'
-import AddCard from './components/AddCard'
-import Quiz from './components/Quiz'
-import { TabNavigator,StackNavigator } from 'react-navigation'
-import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import MainNav from './components/TabsComponents'
 
-const Tabs = TabNavigator({
-  Deck: {
-    screen:AllDecks,
-    navigationOptions: {
-      tabBarLabel:'Decks',
-      tabBarIcon: ({tintColor}) => <Ionicons name='ios-bookmarks' size={30} color={tintColor}/>
-    }
-  },
-  NewDeck: {
-    screen:AddNewDeck,
-    navigationOptions: {
-      tabBarLabel:'NewDeck',
-      tabBarIcon: ({tintColor}) => <FontAwesome name='plus-square' size={30} color={tintColor} />
-    }
-  },
-},{
-  navigationOptions:{
-    header:null
-  },
-  tabBarOptions: {
-    activeTintColor: Platform.OS === 'ios' ? 'purple' : 'white',
-    style: {
-      height: 56,
-      backgroundColor: Platform.OS === 'ios' ? 'white' : 'purple',
-      shadowColor: 'rgba(0, 0, 0, 0.24)',
-      shadowRadius:6,
-      shadowOpacity:1
-    }
-  }
-})
-
-const MainNav = StackNavigator({
-  Home:{
-    screen: Tabs
-  },
-  DeckDetail: {
-    screen:DeckDetail
-  },
-  Quiz: {
-    screen:Quiz
-  },
-  AddCard: {
-    screen:AddCard
-  }
-},{
-  initialRouteName: 'Home',
-});
 
 export default class App extends React.Component {
   render() {
---
+++ components/TabsComponents.js
@@ -0,0 +1,57 @@
+import { TabNavigator,StackNavigator } from 'react-navigation'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import AddNewDeck from './AddNewDeck'
+import AllDecks from './AllDecks'
+import DeckDetail from './DeckDetail'
+import AddCard from './AddCard'
+import Quiz from './Quiz'
+
+const Tabs = TabNavigator({
+  Deck: {
+    screen:AllDecks,
+    navigationOptions: {
+      tabBarLabel:'Decks',
+      tabBarIcon: ({tintColor}) => <Ionicons name='ios-bookmarks' size={30} color={tintColor}/>
+    }
+  },
+  NewDeck: {
+    screen:AddNewDeck,
+    navigationOptions: {
+      tabBarLabel:'NewDeck',
+      tabBarIcon: ({tintColor}) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+    }
+  },
+},{
+  navigationOptions:{
+    header:null
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? 'purple' : 'white',
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? 'white' : 'purple',
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowRadius:6,
+      shadowOpacity:1
+    }
+  }
+})
+
+const MainNav = StackNavigator({
+  Home:{
+    screen: Tabs
+  },
+  DeckDetail: {
+    screen:DeckDetail
+  },
+  Quiz: {
+    screen:Quiz
+  },
+  AddCard: {
+    screen:AddCard
+  }
+},{
+  initialRouteName: 'Home',
+});
+
+export default MainNav
